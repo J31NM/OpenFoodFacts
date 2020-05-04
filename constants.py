7118d22f2297474c4b9b8a1000868c52f957bc0d
J31NM <48386404+J31NM@users.noreply.github.com>
2020-05-04 02:22:52 +0200

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,5 +1,7 @@
-MYSQL_USER = "";
-MYSQL_PWD = "";
-MYSQL_HOST = "";
-MYSQL_DATABASE = "openfoodfacts_db";
+import os
+
+MYSQL_USER = os.environ.get('OC5_MYSQL_USER', 'root')
+MYSQL_PWD = os.environ.get('OC5_MYSQL_PWD', '')
+MYSQL_HOST = os.environ.get('OC5_MYSQL_HOST', '127.0.0.1')
+MYSQL_DATABASE = 'openfoodfacts_db'
 # pylint: disable=W0614
